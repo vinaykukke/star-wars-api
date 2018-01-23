f75ee54b039f006f3d7e84e582a468d1c3fbf64b
vinay kukke <kukke06@gmail.com>
2018-01-23 14:26:56 +0100

    Show example of new typescript interface usage in comment

--- src/client/types/globalTypes.ts
+++ src/client/types/globalTypes.ts
@@ -1,3 +1,11 @@
 export interface IState {
   home: any;
 }
+
+// This is like acts like a function
+// USAGE: ICommunicator<IEmail>, will result in
+// send: (item: IEmail) => void;
+//
+// export interface ICommunicator<T> {
+//   send: (item: T) => void,
+// }
