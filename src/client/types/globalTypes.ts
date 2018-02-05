f574c723d5b63c489f3e3b5a717548179ca324a3
vinaykukke <kukke06@gmail.com>
2018-02-05 21:15:45 +0100

    Update global types
    Correcting the comments

--- src/client/types/globalTypes.ts
+++ src/client/types/globalTypes.ts
@@ -2,7 +2,7 @@ export interface IState {
   home: any;
 }
 
-// This is like acts like a function
+// This acts like a function
 // USAGE: ICommunicator<IEmail>, will result in
 // send: (item: IEmail) => void;
 //
